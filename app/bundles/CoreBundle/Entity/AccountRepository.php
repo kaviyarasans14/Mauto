67955332e5c1516aa73de471ab237c0bd58e97ab
kaviyarasans14 <kaviyarasans14@gmail.com>
2018-03-07 12:01:26 +0530

    Settings and Configuration related Enhancement

---
+++ app/bundles/CoreBundle/Entity/AccountRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributorcomp. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Entity;
+
+/**
+ * Class AccountRepository.
+ */
+class AccountRepository extends CommonRepository
+{
+    /**
+     * {@inhertidoc}.
+     *
+     * @param array $args
+     *
+     * @return Paginator
+     */
+    public function getEntities(array $args = [])
+    {
+        $q = $this
+            ->createQueryBuilder('a')
+            ->select('a');
+        $args['qb'] = $q;
+
+        return parent::getEntities($args);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getTableAlias()
+    {
+        return 'a';
+    }
+}
