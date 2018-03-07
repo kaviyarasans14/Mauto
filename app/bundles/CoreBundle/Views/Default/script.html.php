c5829f95c825e1a9dcef6fc245ba9037ac158bfd
kaviyarasans14 <kaviyarasans14@gmail.com>
2018-03-07 11:56:56 +0530

    Settings and Configuration related Enhancement

--- app/bundles/CoreBundle/Views/Default/script.html.php
+++ app/bundles/CoreBundle/Views/Default/script.html.php
@@ -24,7 +24,10 @@ $mauticContent = $view['slots']->get(
     var mauticEnv         = '<?php echo $app->getEnvironment(); ?>';
     var leClientID        = '<?php echo $view['assets']->getAppid(); ?>';
     var mauticLang        = <?php echo $view['translator']->getJsLang(); ?>;
-
+    document.addEventListener("contextmenu", function(e){
+        e.preventDefault();
+    }, false);
 </script>
+
 <?php $view['assets']->outputSystemScripts(true); ?>
 <?php $view['assets']->outputBeeEditorScripts(); ?>
