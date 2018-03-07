82d6ca3866712ea0a6ac449d7bfa8bc9d33e8cce
kaviyarasans14 <kaviyarasans14@gmail.com>
2018-03-07 12:12:10 +0530

    Settings and Configuration related Enhancements

---
+++ app/migrations/Version20180306143715.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cratio
+ * Date: 23/2/18
+ * Time: 11:21 AM.
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\SkipMigrationException;
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+class Version20180306143715 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        if ($schema->getTable(MAUTIC_TABLE_PREFIX.'email_stats')->hasColumn('is_unsubscribe')) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("ALTER TABLE {$this->prefix}reports ADD is_unsubscribe TINYINT(1) DEFAULT 0");
+        $this->addSql("ALTER TABLE {$this->prefix}reports ADD is_bounce TINYINT(1) DEFAULT 0");
+    }
+}
