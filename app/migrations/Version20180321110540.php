f06203dcfae16e07a8d0f2d2b93f879e08626476
vivekssharma07 <vivekssharma07@gmail.com>
2018-03-21 11:30:48 +0530

    migration for licenseinfo

--- app/migrations/Version20180321110540.php
+++ app/migrations/Version20180321110540.php
@@ -59,7 +59,7 @@ SQL;
         $this->addSql($sql);
         $sql = <<<SQL
 INSERT INTO {$this->prefix}licenseinfo
-  VALUES ('UL',0,'UL',0,'0','UL','UL','2018-03-21','','UL',0,'','','Active')
+  VALUES (1,'UL','0','UL','0','0','UL','UL','2018-03-21','','UL','0','','','Active')
 SQL;
         $this->addSql($sql);
     }
