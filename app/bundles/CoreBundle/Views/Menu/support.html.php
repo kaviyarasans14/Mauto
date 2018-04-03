a5e4bf8633a8de7df8a077f13de2cb4e831b57bd
vivekssharma07 <vivekssharma07@gmail.com>
2018-04-03 13:06:09 +0530

    Leadsengage QC issues

--- app/bundles/CoreBundle/Views/Menu/support.html.php
+++ app/bundles/CoreBundle/Views/Menu/support.html.php
@@ -1,6 +1,6 @@
 <li class="dropdown">
     <a class="btn btn-nospin btn-primary btn-sm hidden-xs" style="margin-top: 17px;font-weight: 700;padding: 5px 10px;font-size: 11px;line-height: 1.456;border-radius: 2px;color: #fff;background-color: #375695;border-color: #375695;" data-toggle="dropdown" href="#">
-        <span><i class="fa fa-question-circle"></i> <span>Help</span></span>
+        <span><i class="fa fa-question-circle"style="font-size: 13px;" ></i> <span>Help</span></span>
     </a>
     <ul class="dropdown-menu dropdown-menu-right">
         <li>
