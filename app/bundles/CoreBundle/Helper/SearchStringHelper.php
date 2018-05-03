51786dd7f742cf06b69c00f8ab5450f17a7bb29a
sprabubtech <sprabubtech@gmail.com>
2018-05-03 17:11:41 +0530

    payment related changes and minor issues

--- app/bundles/CoreBundle/Helper/SearchStringHelper.php
+++ app/bundles/CoreBundle/Helper/SearchStringHelper.php
@@ -186,7 +186,7 @@ class SearchStringHelper
                     $filters->{$baseName}[$keyCount]->command = $command;
                     $string                                   = '';
                 }
-            } elseif ($char == ' ' && false) { //disabled by prabhu
+            } elseif ($char == ' ') {
                 //arrived at the end of a single word that is not within a quote or parenthesis so add it as standalone
                 if ($string != ' ') {
                     $string = trim($string);
